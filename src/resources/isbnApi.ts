326ad5d20c063ec553f18b0156449111de42535c
Matej Bukacek <493307@mail.muni.cz>
2022-05-23 22:55:36 +0200

    reverting changes on main

--- src/resources/isbnApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { Book } from "../types/book";
-
-export const getBookByISBN = async (isbn: string): Promise<Book> => {
-  const result = await axios.get(`http://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`);
-  const book: Book = result.data[`ISBN:${isbn}`];
-  return book;
-};
