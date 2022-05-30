da51ebb9286a9ab2b4f71fa434f963afb6c6e7bf
Ladislav Husty <x477390@fi.muni.cz>
2022-05-30 18:38:43 +0200

    ISBN: Accordion done waiting for MainPage setup to be merged from other branch

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Error404 } from "./error/error404";
 import { MainPage } from "./MainPage";
-
+import { Isbn } from "./isbn/isbn";
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/isbn" element={<Isbn />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
