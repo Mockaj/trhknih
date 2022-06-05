1b4d410e0759f61fbccfd976b44a1253d8f3f532
Matěj Bukáček <xbukacek@fi.muni.cz>
2022-06-05 17:08:19 +0200

    Frontend book info

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -7,37 +7,46 @@ import { Registration } from "./Registration&Login/Registration";
 import { Faq } from "./FAQ/faq";
 import { Cart } from "./Cart/Cart";
 import { SellBook } from "./SellBook/SellBook";
+import { RecoilRoot } from "recoil";
+import { BookInfo } from "./BookInfo/BookInfo";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage Content={<BasePage />} />} />
-        <Route path="/cart" element={<MainPage Content={<Cart />} />} />
-        <Route
-          path="/register"
-          element={<MainPage Content={<Registration />} />}
-        />
-        <Route path="/login" element={<MainPage Content={<Login />} />} />
-        <Route
-          path="/faq"
-          element={<MainPage Content={<Faq topic="faq" />} />}
-        />
-        <Route
-          path="/how-to-sell-a-book"
-          element={<MainPage Content={<Faq topic="sell" />} />}
-        />
-        <Route
-          path="/what-is-isbn"
-          element={<MainPage Content={<Faq topic="isbn" />} />}
-        />
-        <Route
-          path="/sell-a-book"
-          element={<MainPage Content={<SellBook />} />}
-        />
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage Content={<BasePage />} />} />
+          <Route path="/cart" element={<MainPage Content={<Cart />} />} />
+          <Route
+            path="/register"
+            element={<MainPage Content={<Registration />} />}
+          />
+          <Route path="/login" element={<MainPage Content={<Login />} />} />
+          <Route
+            path="/faq"
+            element={<MainPage Content={<Faq topic="faq" />} />}
+          />
+          <Route
+            path="/how-to-sell-a-book"
+            element={<MainPage Content={<Faq topic="sell" />} />}
+          />
+          <Route
+            path="/what-is-isbn"
+            element={<MainPage Content={<Faq topic="isbn" />} />}
+          />
+          <Route
+            path="/sell-a-book"
+            element={<MainPage Content={<SellBook />} />}
+          />
+          <Route
+            path="/books/:id"
+            element={<MainPage Content={<BookInfo />} />}
+          />
 
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </BrowserRouter>
+
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
