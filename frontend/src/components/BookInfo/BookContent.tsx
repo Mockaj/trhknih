6957e7c0265e9dc27a4310d5b246bfc425fe9e8d
Matej Bukacek <493307@mail.muni.cz>
2022-06-09 18:19:49 +0200

    book-info communicates with database

---
+++ frontend/src/components/BookInfo/BookContent.tsx
@@ -0,0 +1,84 @@
+import { Link } from "react-router-dom"
+
+export const BookContent = (props) => {
+
+  const BookPage = (props) => {
+    const {data} = props
+    const categories: any[] = []
+
+    if (data) {
+      console.log(data)
+      console.log(data.data.book)
+      return (
+        <div className="book-info">
+      <div className="book-info__picture-part">
+        <div className="mobile-only">
+          <h2 className="book-info__title">{data.data.book.title}</h2>
+          {data.data.book.fromAutors.map((author) => { return (
+            <p className="book-info__author">{author.author.name}</p> ) })
+          }
+
+        </div>
+        <div className="book-cover-wrapper">
+          <img src={data.data.book.photo} alt="BookCover" />
+        </div>
+      </div>
+
+      <div className="book-info__text-part">
+        <div className="book-info__text-part-wrapper">
+        <div className="desktop-only">
+          <h2 className="book-info__title">{data.data.book.title}</h2>
+          {data.data.book.fromAutors.map((author) => { return (
+            <p className="book-info__author">{author.author.name}</p> ) })
+          }
+        </div>
+        <div className="price-wrapper">
+          <p className="price-wrapper__price">{data.data.price} €</p>
+          <button className="price-wrapper__button">Add to cart</button>
+        </div>
+        <div className="info">
+          <div className="info__part-wrapper">
+            <div className="info__info-part">
+              <p className="info__label">Publish&nbsp;year:</p>
+              <p className="info__label-value">{data.data.book.publishedDate}</p>
+            </div>
+            <div className="info__info-part">
+              <p className="info__label">Publisher:</p>
+              <p className="info__label-value">{data.data.book.publisher.name}</p>
+            </div>
+          </div>
+          <div className="info__part-wrapper">
+          <div className="info__info-part">
+              <p className="info__label">Pages:</p>
+              <p className="info__label-value">{data.data.book.numberOfPages}</p>
+            </div>
+            <div className="info__info-part">
+              <p className="info__label">Seller:</p>
+              <p className="info__label-value">{data.data.seller.username}</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div className="book-info__categories">
+          {categories.map((category) => 
+            <Link  className="book-info__category" to={`/categories?category=${category}`}>{category}</Link>)
+          }
+        </div>
+      </div>
+    </div>
+      )
+    } else {
+      return (
+        <h3>
+          loading
+        </h3>
+      )
+    }
+  } 
+
+  return(
+    <>
+      {BookPage(props)}
+    </>
+  )
+}
--- frontend/src/components/BookInfo/BookInfo.tsx
+++ frontend/src/components/BookInfo/BookInfo.tsx
@@ -1,83 +1,34 @@
 import { useDocumentTitle } from "@mantine/hooks";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { BookContent } from "./BookContent";
 import "./BookInfo.css"
 
-export interface BookInfoProps {
-  id: number;
-  image: string;
-  name: string;
-  author: string;
-  price: number;
-}
-
-const getBookInfo = async (id:string) => {
-  const result = await axios.get(`http://localhost:4000/api/offers/${id}`);
-  return result
-}
 
 export const BookInfo = () => {
   useDocumentTitle("BookNameFromApi \u00B7 Readee - recycle books")
-
   const params = useParams()
-  
-  // params.id --> API get request
-  console.log(getBookInfo("802e22be-7094-4165-8996-dfa7c9f2c7cd"));
 
-  const categories = [ "category1", "category2", "category3", "category4" ]
-  const image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/5750/9780575089914.jpg"
+  const [data, setData] = useState()
+  // params.id
+  const id = "802e22be-7094-4165-8996-dfa7c9f2c7cd"
+  const url = `http://localhost:4000/api/offers/${id}`
 
-  return (
-    <div className="book-info">
-      <div className="book-info__picture-part">
-        <div className="mobile-only">
-          <h2 className="book-info__title">TITLE</h2>
-          <p className="book-info__author">AUTHOR</p>
-        </div>
-        <div className="book-cover-wrapper">
-          <img src={image} alt="BookCover" />
-        </div>
-      </div>
+  useEffect(() => {
+    getData();
+  }, []);
 
-      <div className="book-info__text-part">
-        <div className="book-info__text-part-wrapper">
-        <div className="desktop-only">
-          <h2 className="book-info__title">TITLE</h2>
-          <p className="book-info__author">AUTHOR</p>
-        </div>
-        <div className="price-wrapper">
-          <p className="price-wrapper__price">PRICE</p>
-          <button className="price-wrapper__button">Add to cart</button>
-        </div>
-        <div className="info">
-          <div className="info__part-wrapper">
-            <div className="info__info-part">
-              <p className="info__label">Publish&nbsp;year:</p>
-              <p className="info__label-value">YEAR</p>
-            </div>
-            <div className="info__info-part">
-              <p className="info__label">Publisher:</p>
-              <p className="info__label-value">PUBLISHER</p>
-            </div>
-          </div>
-          <div className="info__part-wrapper">
-          <div className="info__info-part">
-              <p className="info__label">Pages:</p>
-              <p className="info__label-value">PAGES</p>
-            </div>
-            <div className="info__info-part">
-              <p className="info__label">Seller:</p>
-              <p className="info__label-value">USERNAME</p>
-            </div>
-          </div>
-        </div>
-        </div>
-        <div className="book-info__categories">
-          {categories.map((category) => 
-            <Link  className="book-info__category" to={`/categories?category=${category}`}>{category}</Link>)
-          }
-        </div>
-      </div>
-    </div>
+  const getData = () => {
+    axios.get(`${url}`)
+    .then((response) => {
+      const data = response.data;
+      setData(data);
+    })
+    .catch(error => console.log(`Error: ${error}`));
+  }
+
+  return(
+    <BookContent data={data} />
   )
 }
