52fdc9709d9e6a15dc51bb2b7e06a484331d8be1
Ladislav Husty <x477390@fi.muni.cz>
2022-06-14 21:42:15 +0200

    BUG FIX: Clear filter button now clears multiselect text as well

--- frontend/src/components/categories/CategoriesSidebar.tsx
+++ frontend/src/components/categories/CategoriesSidebar.tsx
@@ -1,33 +1,36 @@
-import { MultiSelect } from "react-multi-select-component"
-import { Link, useLocation } from "react-router-dom"
-
+import { MultiSelect } from "react-multi-select-component";
+import { Link, useLocation } from "react-router-dom";
 
 export const CategoriesSidebar = (props) => {
-  const {options, selected, setSelected} = props
-  const location = useLocation()
+  const { options, selected, setSelected } = props;
+  const location = useLocation();
   const query = new URLSearchParams(location.search);
 
   const categories = [
     ["Bestsellers", "bestsellers"],
     ["Newly added", "newlyAdded"],
-    ["Free books", "freeBooks"]
-  ]
-  
+    ["Free books", "freeBooks"],
+  ];
+  let clearButton = document.getElementsByClassName("clear-selected-button");
+
   return (
     <nav className="categories-sidebar-container">
       <h1>Books</h1>
-        <>
-          <h3 className="categories__subheading">Featured</h3>
-          <ul className="categories-featured__list">
-            {categories.map((category) => (
-              <li key={category[0]} className="categories-featured__item">
-                <Link className="categories-featured__item-link" to={`/categories/${category[1]}/1${location.search}`}>
-                  {category[0]}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </>
+      <>
+        <h3 className="categories__subheading">Featured</h3>
+        <ul className="categories-featured__list">
+          {categories.map((category) => (
+            <li key={category[0]} className="categories-featured__item">
+              <Link
+                className="categories-featured__item-link"
+                to={`/categories/${category[1]}/1${location.search}`}
+              >
+                {category[0]}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
 
       <div>
         <h3 className="categories__subheading">Tags</h3>
@@ -39,14 +42,23 @@ export const CategoriesSidebar = (props) => {
             onChange={setSelected}
             labelledBy="Select"
           />
-          <Link className="categories-apply-filters" to={`?search=${query.get("search")}&tags=${selected.map((tag) => `${tag.label}`)}`} >
+          <Link
+            className="categories-apply-filters"
+            to={`?search=${query.get("search")}&tags=${selected.map(
+              (tag) => `${tag.label}`
+            )}`}
+          >
             Apply filters
           </Link>
-          <Link className="categories-apply-filters" to={"?"} >
+          <Link
+            className="categories-apply-filters"
+            to={"?"}
+            onClick={() => clearButton?.click()}
+          >
             Clear filters
           </Link>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
